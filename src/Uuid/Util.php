e3cf334064246ac3e88cc0e07362dc8fca227cf3
Ben Ramsey <ben@benramsey.com>
2022-09-25 14:44:40 -0500

    feat: add utilities for manipulating UUIDs

---
+++ src/Uuid/Util.php
@@ -0,0 +1,118 @@
+<?php
+
+/**
+ * This file is part of ramsey/identifier
+ *
+ * ramsey/identifier is open source software: you can distribute
+ * it and/or modify it under the terms of the MIT License
+ * (the "License"). You may not use this file except in
+ * compliance with the License.
+ *
+ * @copyright Copyright (c) Ben Ramsey <ben@benramsey.com>
+ * @license https://opensource.org/licenses/MIT MIT License
+ */
+
+declare(strict_types=1);
+
+namespace Ramsey\Identifier\Uuid;
+
+use Identifier\Uuid\Variant;
+use Identifier\Uuid\Version;
+use Ramsey\Identifier\Exception\InvalidArgumentException;
+
+use function pack;
+use function strlen;
+use function unpack;
+
+/**
+ * Utilities for manipulating and managing UUIDs
+ */
+final class Util
+{
+    /**
+     * Applies the RFC 4122 version number and variant field to the 128-bit
+     * integer (as a 16-byte string) provided
+     *
+     * @param non-empty-string $bytes A 128-bit integer (16-byte string) to
+     *     which the RFC 4122 version number and variant field will be applied,
+     *     making the number a valid UUID
+     * @param Version | null $version The RFC 4122 version to apply
+     * @param Variant $variant The variant to apply
+     *
+     * @return non-empty-string A 16-byte string with the UUID version and variant applied
+     *
+     * @psalm-pure
+     */
+    public static function applyVersionAndVariant(
+        string $bytes,
+        ?Version $version,
+        Variant $variant = Variant::Rfc4122,
+    ): string {
+        if (strlen($bytes) !== 16) {
+            throw new InvalidArgumentException('$bytes must be a a 16-byte string');
+        }
+
+        /** @var int[] $parts */
+        $parts = unpack('n*', $bytes);
+
+        if ($version !== null) {
+            $parts[4] = self::applyVersion($parts[4], $version);
+        }
+
+        $parts[5] = self::applyVariant($parts[5], $variant);
+
+        /** @var non-empty-string */
+        return pack('n*', ...$parts);
+    }
+
+    /**
+     * Applies the RFC 4122 variant field to the 16-bit clock sequence
+     *
+     * @link http://tools.ietf.org/html/rfc4122#section-4.1.1 RFC 4122, § 4.1.1: Variant
+     *
+     * @param int $clockSeq The 16-bit clock sequence value before the RFC 4122
+     *     variant is applied
+     *
+     * @return int The 16-bit clock sequence multiplexed with the UUID variant
+     *
+     * @psalm-pure
+     */
+    private static function applyVariant(int $clockSeq, Variant $variant): int
+    {
+        return match ($variant) {
+            Variant::ReservedNcs => $clockSeq & 0x7fff,
+            Variant::Rfc4122 => $clockSeq & 0x3fff | 0x8000,
+            Variant::ReservedMicrosoft => $clockSeq & 0x1fff | 0xc000,
+            Variant::ReservedFuture => $clockSeq & 0x1fff | 0xe000,
+        };
+    }
+
+    /**
+     * Applies the RFC 4122 version number to the 16-bit `time_hi_and_version` field
+     *
+     * @link http://tools.ietf.org/html/rfc4122#section-4.1.3 RFC 4122, § 4.1.3: Version
+     *
+     * @param int $timeHi The value of the 16-bit `time_hi_and_version` field
+     *     before the RFC 4122 version is applied
+     * @param Version $version The RFC 4122 version to apply to the `time_hi` field
+     *
+     * @return int The 16-bit time_hi field of the timestamp multiplexed with
+     *     the UUID version number
+     *
+     * @psalm-pure
+     */
+    private static function applyVersion(int $timeHi, Version $version): int
+    {
+        $timeHi = $timeHi & 0x0fff;
+        $timeHi |= $version->value << 12;
+
+        return $timeHi;
+    }
+
+    /**
+     * Disallow public instantiation
+     */
+    private function __construct()
+    {
+    }
+}
---
+++ tests/unit/Uuid/UtilTest.php
@@ -0,0 +1,211 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ramsey\Test\Identifier\Uuid;
+
+use Identifier\Uuid\Variant;
+use Identifier\Uuid\Version;
+use Ramsey\Identifier\Exception\InvalidArgumentException;
+use Ramsey\Identifier\Uuid\Util;
+use Ramsey\Test\Identifier\TestCase;
+
+use function bin2hex;
+use function sprintf;
+
+class UtilTest extends TestCase
+{
+    /**
+     * @param non-empty-string $bytes
+     *
+     * @dataProvider versionAndVariantBytesProvider
+     */
+    public function testApplyVersionAndVariant(
+        string $bytes,
+        ?Version $version,
+        Variant $variant,
+        string $expectedBytes,
+    ): void {
+        $applied = Util::applyVersionAndVariant($bytes, $version, $variant);
+
+        $this->assertSame(
+            $expectedBytes,
+            $applied,
+            sprintf('Expected "%s", received "%s"', bin2hex($expectedBytes), bin2hex($applied)),
+        );
+    }
+
+    /**
+     * @return array<array{bytes: non-empty-string, version: Version | null, variant: Variant, expectedBytes: string}>
+     */
+    public function versionAndVariantBytesProvider(): array
+    {
+        return [
+            [
+                'bytes' => "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff",
+                'version' => Version::GregorianTime,
+                'variant' => Variant::Rfc4122,
+                'expectedBytes' => "\xff\xff\xff\xff\xff\xff\x1f\xff\xbf\xff\xff\xff\xff\xff\xff\xff",
+            ],
+            [
+                'bytes' => "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff",
+                'version' => Version::DceSecurity,
+                'variant' => Variant::Rfc4122,
+                'expectedBytes' => "\xff\xff\xff\xff\xff\xff\x2f\xff\xbf\xff\xff\xff\xff\xff\xff\xff",
+            ],
+            [
+                'bytes' => "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff",
+                'version' => Version::HashMd5,
+                'variant' => Variant::Rfc4122,
+                'expectedBytes' => "\xff\xff\xff\xff\xff\xff\x3f\xff\xbf\xff\xff\xff\xff\xff\xff\xff",
+            ],
+            [
+                'bytes' => "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff",
+                'version' => Version::Random,
+                'variant' => Variant::Rfc4122,
+                'expectedBytes' => "\xff\xff\xff\xff\xff\xff\x4f\xff\xbf\xff\xff\xff\xff\xff\xff\xff",
+            ],
+            [
+                'bytes' => "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff",
+                'version' => Version::HashSha1,
+                'variant' => Variant::Rfc4122,
+                'expectedBytes' => "\xff\xff\xff\xff\xff\xff\x5f\xff\xbf\xff\xff\xff\xff\xff\xff\xff",
+            ],
+            [
+                'bytes' => "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff",
+                'version' => Version::ReorderedGregorianTime,
+                'variant' => Variant::Rfc4122,
+                'expectedBytes' => "\xff\xff\xff\xff\xff\xff\x6f\xff\xbf\xff\xff\xff\xff\xff\xff\xff",
+            ],
+            [
+                'bytes' => "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff",
+                'version' => Version::UnixTime,
+                'variant' => Variant::Rfc4122,
+                'expectedBytes' => "\xff\xff\xff\xff\xff\xff\x7f\xff\xbf\xff\xff\xff\xff\xff\xff\xff",
+            ],
+            [
+                'bytes' => "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff",
+                'version' => Version::Custom,
+                'variant' => Variant::Rfc4122,
+                'expectedBytes' => "\xff\xff\xff\xff\xff\xff\x8f\xff\xbf\xff\xff\xff\xff\xff\xff\xff",
+            ],
+            [
+                'bytes' => "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00",
+                'version' => Version::Random,
+                'variant' => Variant::Rfc4122,
+                'expectedBytes' => "\x00\x00\x00\x00\x00\x00\x40\x00\x80\x00\x00\x00\x00\x00\x00\x00",
+            ],
+            [
+                'bytes' => "\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11",
+                'version' => Version::Random,
+                'variant' => Variant::Rfc4122,
+                'expectedBytes' => "\x11\x11\x11\x11\x11\x11\x41\x11\x91\x11\x11\x11\x11\x11\x11\x11",
+            ],
+            [
+                'bytes' => "\x22\x22\x22\x22\x22\x22\x22\x22\x22\x22\x22\x22\x22\x22\x22\x22",
+                'version' => Version::Random,
+                'variant' => Variant::Rfc4122,
+                'expectedBytes' => "\x22\x22\x22\x22\x22\x22\x42\x22\xa2\x22\x22\x22\x22\x22\x22\x22",
+            ],
+            [
+                'bytes' => "\x33\x33\x33\x33\x33\x33\x33\x33\x33\x33\x33\x33\x33\x33\x33\x33",
+                'version' => Version::Random,
+                'variant' => Variant::Rfc4122,
+                'expectedBytes' => "\x33\x33\x33\x33\x33\x33\x43\x33\xb3\x33\x33\x33\x33\x33\x33\x33",
+            ],
+            [
+                'bytes' => "\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44",
+                'version' => Version::Random,
+                'variant' => Variant::Rfc4122,
+                'expectedBytes' => "\x44\x44\x44\x44\x44\x44\x44\x44\x84\x44\x44\x44\x44\x44\x44\x44",
+            ],
+            [
+                'bytes' => "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00",
+                'version' => null,
+                'variant' => Variant::ReservedNcs,
+                'expectedBytes' => "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00",
+            ],
+            [
+                'bytes' => "\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11",
+                'version' => null,
+                'variant' => Variant::ReservedNcs,
+                'expectedBytes' => "\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11",
+            ],
+            [
+                'bytes' => "\x88\x88\x88\x88\x88\x88\x88\x88\x88\x88\x88\x88\x88\x88\x88\x88",
+                'version' => null,
+                'variant' => Variant::ReservedNcs,
+                'expectedBytes' => "\x88\x88\x88\x88\x88\x88\x88\x88\x08\x88\x88\x88\x88\x88\x88\x88",
+            ],
+            [
+                'bytes' => "\x99\x99\x99\x99\x99\x99\x99\x99\x99\x99\x99\x99\x99\x99\x99\x99",
+                'version' => null,
+                'variant' => Variant::ReservedNcs,
+                'expectedBytes' => "\x99\x99\x99\x99\x99\x99\x99\x99\x19\x99\x99\x99\x99\x99\x99\x99",
+            ],
+            [
+                'bytes' => "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00",
+                'version' => null,
+                'variant' => Variant::ReservedMicrosoft,
+                'expectedBytes' => "\x00\x00\x00\x00\x00\x00\x00\x00\xc0\x00\x00\x00\x00\x00\x00\x00",
+            ],
+            [
+                'bytes' => "\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11",
+                'version' => null,
+                'variant' => Variant::ReservedMicrosoft,
+                'expectedBytes' => "\x11\x11\x11\x11\x11\x11\x11\x11\xd1\x11\x11\x11\x11\x11\x11\x11",
+            ],
+            [
+                'bytes' => "\x88\x88\x88\x88\x88\x88\x88\x88\x88\x88\x88\x88\x88\x88\x88\x88",
+                'version' => null,
+                'variant' => Variant::ReservedMicrosoft,
+                'expectedBytes' => "\x88\x88\x88\x88\x88\x88\x88\x88\xc8\x88\x88\x88\x88\x88\x88\x88",
+            ],
+            [
+                'bytes' => "\x99\x99\x99\x99\x99\x99\x99\x99\x99\x99\x99\x99\x99\x99\x99\x99",
+                'version' => null,
+                'variant' => Variant::ReservedMicrosoft,
+                'expectedBytes' => "\x99\x99\x99\x99\x99\x99\x99\x99\xd9\x99\x99\x99\x99\x99\x99\x99",
+            ],
+            [
+                'bytes' => "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00",
+                'version' => null,
+                'variant' => Variant::ReservedFuture,
+                'expectedBytes' => "\x00\x00\x00\x00\x00\x00\x00\x00\xe0\x00\x00\x00\x00\x00\x00\x00",
+            ],
+            [
+                'bytes' => "\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11",
+                'version' => null,
+                'variant' => Variant::ReservedFuture,
+                'expectedBytes' => "\x11\x11\x11\x11\x11\x11\x11\x11\xf1\x11\x11\x11\x11\x11\x11\x11",
+            ],
+            [
+                'bytes' => "\x88\x88\x88\x88\x88\x88\x88\x88\x88\x88\x88\x88\x88\x88\x88\x88",
+                'version' => null,
+                'variant' => Variant::ReservedFuture,
+                'expectedBytes' => "\x88\x88\x88\x88\x88\x88\x88\x88\xe8\x88\x88\x88\x88\x88\x88\x88",
+            ],
+            [
+                'bytes' => "\x99\x99\x99\x99\x99\x99\x99\x99\x99\x99\x99\x99\x99\x99\x99\x99",
+                'version' => null,
+                'variant' => Variant::ReservedFuture,
+                'expectedBytes' => "\x99\x99\x99\x99\x99\x99\x99\x99\xf9\x99\x99\x99\x99\x99\x99\x99",
+            ],
+            [
+                'bytes' => "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00",
+                'version' => null,
+                'variant' => Variant::Rfc4122,
+                'expectedBytes' => "\x00\x00\x00\x00\x00\x00\x00\x00\x80\x00\x00\x00\x00\x00\x00\x00",
+            ],
+        ];
+    }
+
+    public function testApplyVersionAndVariantThrowsExceptionWhenBytesAreWrongLength(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('$bytes must be a a 16-byte string');
+
+        /** @phpstan-ignore-next-line */
+        Util::applyVersionAndVariant('foobar', null);
+    }
+}
