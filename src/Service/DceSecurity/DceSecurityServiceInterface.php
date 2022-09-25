6f404a5092fd4004581854bb92ee72ec39d8fea7
Ben Ramsey <ben@benramsey.com>
2022-09-25 12:13:47 -0500

    feat: add DCE Security service

---
+++ src/Service/DceSecurity/DceSecurityServiceInterface.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * This file is part of ramsey/identifier
+ *
+ * ramsey/identifier is open source software: you can distribute
+ * it and/or modify it under the terms of the MIT License
+ * (the "License"). You may not use this file except in
+ * compliance with the License.
+ *
+ * @copyright Copyright (c) Ben Ramsey <ben@benramsey.com>
+ * @license https://opensource.org/licenses/MIT MIT License
+ */
+
+declare(strict_types=1);
+
+namespace Ramsey\Identifier\Service\DceSecurity;
+
+use Ramsey\Identifier\Exception\DceSecurityException;
+use Ramsey\Identifier\Uuid\UuidV2;
+
+/**
+ * Defines a service interface for getting system group, person, and
+ * organization identifiers for generating DCE Security identifiers
+ *
+ * @see UuidV2
+ */
+interface DceSecurityServiceInterface
+{
+    /**
+     * Returns a group identifier for the system
+     *
+     * @link https://en.wikipedia.org/wiki/Group_identifier Group identifier
+     *
+     * @return int<0, max>
+     *
+     * @throws DceSecurityException when unable to find a group identifier
+     */
+    public function getGroupIdentifier(): int;
+
+    /**
+     * Returns an organization identifier
+     *
+     * @return int<0, max>
+     *
+     * @throws DceSecurityException when unable to find an organization identifier
+     */
+    public function getOrgIdentifier(): int;
+
+    /**
+     * Returns a user identifier for the system
+     *
+     * @link https://en.wikipedia.org/wiki/User_identifier User identifier
+     *
+     * @return int<0, max>
+     *
+     * @throws DceSecurityException when unable to find a person/user identifier
+     */
+    public function getPersonIdentifier(): int;
+}
---
+++ tests/unit/Service/DceSecurity/SystemDceSecurityServiceTest.php
@@ -0,0 +1,220 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ramsey\Test\Identifier\Service\DceSecurity;
+
+use Hamcrest\Type\IsInteger;
+use InvalidArgumentException as PhpInvalidArgumentException;
+use Psr\SimpleCache\CacheInterface;
+use Psr\SimpleCache\InvalidArgumentException as CacheInvalidArgumentException;
+use Ramsey\Identifier\Exception\CacheException;
+use Ramsey\Identifier\Exception\DceSecurityException;
+use Ramsey\Identifier\Service\DceSecurity\SystemDceSecurityService;
+use Ramsey\Test\Identifier\TestCase;
+
+use function sprintf;
+
+class SystemDceSecurityServiceTest extends TestCase
+{
+    public function testGetOrgIdentifier(): void
+    {
+        $service = new SystemDceSecurityService(orgId: 4001);
+
+        $this->assertSame(4001, $service->getOrgIdentifier());
+    }
+
+    public function testGetOrgIdentifierThrowsExceptionForMissingId(): void
+    {
+        $service = new SystemDceSecurityService();
+
+        $this->expectException(DceSecurityException::class);
+        $this->expectExceptionMessage(
+            'To use the org identifier, you must set $orgId when instantiating ' . SystemDceSecurityService::class,
+        );
+
+        $service->getOrgIdentifier();
+    }
+
+    /**
+     * @runInSeparateProcess since the identifier is stored statically on the class
+     * @preserveGlobalState disabled
+     */
+    public function testGetGroupIdentifier(): void
+    {
+        $service = new SystemDceSecurityService();
+
+        $this->assertGreaterThanOrEqual(0, $service->getGroupIdentifier());
+    }
+
+    /**
+     * @runInSeparateProcess since the identifier is stored statically on the class
+     * @preserveGlobalState disabled
+     */
+    public function testGetGroupIdentifierFromCache(): void
+    {
+        $cache = $this->mockery(CacheInterface::class);
+        $cache->expects()->get(SystemDceSecurityService::class . '::$groupId')->andReturns(5001);
+
+        $service = new SystemDceSecurityService(cache: $cache);
+
+        $this->assertSame(5001, $service->getGroupIdentifier());
+
+        // Assert subsequent calls do not attempt to fetch from cache.
+        $this->assertSame(5001, $service->getGroupIdentifier());
+    }
+
+    /**
+     * @runInSeparateProcess since the identifier is stored statically on the class
+     * @preserveGlobalState disabled
+     */
+    public function testGetGroupIdentifierFromCacheThrowsExceptionForBadCacheKey(): void
+    {
+        $cache = $this->mockery(CacheInterface::class);
+        $cache->expects()->get(SystemDceSecurityService::class . '::$groupId')->andThrows(
+            new class extends PhpInvalidArgumentException implements CacheInvalidArgumentException {
+            },
+        );
+
+        $service = new SystemDceSecurityService(cache: $cache);
+
+        $this->expectException(CacheException::class);
+        $this->expectExceptionMessage(sprintf(
+            'A problem occurred when attempting to use the cache key "%s"',
+            SystemDceSecurityService::class . '::$groupId',
+        ));
+
+        $service->getGroupIdentifier();
+    }
+
+    /**
+     * @runInSeparateProcess since the identifier is stored statically on the class
+     * @preserveGlobalState disabled
+     */
+    public function testGetGroupIdentifierFromCacheSetsIdentifierOnCache(): void
+    {
+        $cache = $this->mockery(CacheInterface::class);
+        $cache->expects()->get(SystemDceSecurityService::class . '::$groupId')->andReturns(null);
+        $cache->expects()->set(SystemDceSecurityService::class . '::$groupId', new IsInteger())->andReturns(true);
+
+        $service = new SystemDceSecurityService(cache: $cache);
+        $groupId = $service->getGroupIdentifier();
+
+        $this->assertGreaterThanOrEqual(0, $groupId);
+
+        // Assert subsequent calls do not attempt to fetch from cache.
+        $this->assertSame($groupId, $service->getGroupIdentifier());
+    }
+
+    /**
+     * @runInSeparateProcess since the identifier is stored statically on the class
+     * @preserveGlobalState disabled
+     */
+    public function testGetGroupIdentifierThrowsExceptionWhenIdentifierNotFound(): void
+    {
+        $cache = $this->mockery(CacheInterface::class);
+        $cache->expects()->get(SystemDceSecurityService::class . '::$groupId')->andReturns(-1);
+
+        $service = new SystemDceSecurityService(cache: $cache);
+
+        $this->expectException(DceSecurityException::class);
+        $this->expectExceptionMessage(
+            'Unable to get a group identifier using the system DCE '
+            . 'Security service; please provide a custom identifier or use '
+            . 'a different DCE service class',
+        );
+
+        $service->getGroupIdentifier();
+    }
+
+    /**
+     * @runInSeparateProcess since the identifier is stored statically on the class
+     * @preserveGlobalState disabled
+     */
+    public function testGetPersonIdentifier(): void
+    {
+        $service = new SystemDceSecurityService();
+
+        $this->assertGreaterThanOrEqual(0, $service->getPersonIdentifier());
+    }
+
+    /**
+     * @runInSeparateProcess since the identifier is stored statically on the class
+     * @preserveGlobalState disabled
+     */
+    public function testGetPersonIdentifierFromCache(): void
+    {
+        $cache = $this->mockery(CacheInterface::class);
+        $cache->expects()->get(SystemDceSecurityService::class . '::$personId')->andReturns(6001);
+
+        $service = new SystemDceSecurityService(cache: $cache);
+
+        $this->assertSame(6001, $service->getPersonIdentifier());
+
+        // Assert subsequent calls do not attempt to fetch from cache.
+        $this->assertSame(6001, $service->getPersonIdentifier());
+    }
+
+    /**
+     * @runInSeparateProcess since the identifier is stored statically on the class
+     * @preserveGlobalState disabled
+     */
+    public function testGetPersonIdentifierFromCacheThrowsExceptionForBadCacheKey(): void
+    {
+        $cache = $this->mockery(CacheInterface::class);
+        $cache->expects()->get(SystemDceSecurityService::class . '::$personId')->andThrows(
+            new class extends PhpInvalidArgumentException implements CacheInvalidArgumentException {
+            },
+        );
+
+        $service = new SystemDceSecurityService(cache: $cache);
+
+        $this->expectException(CacheException::class);
+        $this->expectExceptionMessage(sprintf(
+            'A problem occurred when attempting to use the cache key "%s"',
+            SystemDceSecurityService::class . '::$personId',
+        ));
+
+        $service->getPersonIdentifier();
+    }
+
+    /**
+     * @runInSeparateProcess since the identifier is stored statically on the class
+     * @preserveGlobalState disabled
+     */
+    public function testGetPersonIdentifierFromCacheSetsIdentifierOnCache(): void
+    {
+        $cache = $this->mockery(CacheInterface::class);
+        $cache->expects()->get(SystemDceSecurityService::class . '::$personId')->andReturns(null);
+        $cache->expects()->set(SystemDceSecurityService::class . '::$personId', new IsInteger())->andReturns(true);
+
+        $service = new SystemDceSecurityService(cache: $cache);
+        $personId = $service->getPersonIdentifier();
+
+        $this->assertGreaterThanOrEqual(0, $personId);
+
+        // Assert subsequent calls do not attempt to fetch from cache.
+        $this->assertSame($personId, $service->getPersonIdentifier());
+    }
+
+    /**
+     * @runInSeparateProcess since the identifier is stored statically on the class
+     * @preserveGlobalState disabled
+     */
+    public function testGetPersonIdentifierThrowsExceptionWhenIdentifierNotFound(): void
+    {
+        $cache = $this->mockery(CacheInterface::class);
+        $cache->expects()->get(SystemDceSecurityService::class . '::$personId')->andReturns(-1);
+
+        $service = new SystemDceSecurityService(cache: $cache);
+
+        $this->expectException(DceSecurityException::class);
+        $this->expectExceptionMessage(
+            'Unable to get a person identifier using the system DCE '
+            . 'Security service; please provide a custom identifier or use '
+            . 'a different DCE service class',
+        );
+
+        $service->getPersonIdentifier();
+    }
+}
