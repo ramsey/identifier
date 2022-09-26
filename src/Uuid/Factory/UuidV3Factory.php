e3823baa5e321a2bf1410cb0ca1d3787c482f0c2
Ben Ramsey <ben@benramsey.com>
2022-09-25 21:23:15 -0500

    feat: add factories for name-based UUIDs

---
+++ src/Uuid/Factory/UuidV3Factory.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * This file is part of ramsey/identifier
+ *
+ * ramsey/identifier is open source software: you can distribute
+ * it and/or modify it under the terms of the MIT License
+ * (the "License"). You may not use this file except in
+ * compliance with the License.
+ *
+ * @copyright Copyright (c) Ben Ramsey <ben@benramsey.com>
+ * @license https://opensource.org/licenses/MIT MIT License
+ */
+
+declare(strict_types=1);
+
+namespace Ramsey\Identifier\Uuid\Factory;
+
+use Identifier\Uuid\UuidFactoryInterface;
+use Identifier\Uuid\UuidInterface;
+use Identifier\Uuid\Version;
+use Ramsey\Identifier\Exception\InvalidArgumentException;
+use Ramsey\Identifier\Uuid\Util;
+use Ramsey\Identifier\Uuid\UuidV3;
+
+use function hash;
+
+/**
+ * A factory for creating version 3, name-based (MD5) UUIDs
+ *
+ * @psalm-immutable
+ */
+final class UuidV3Factory implements UuidFactoryInterface
+{
+    use DefaultFactory;
+
+    public function create(?UuidInterface $namespace = null, ?string $name = null): UuidV3
+    {
+        if ($namespace === null) {
+            throw new InvalidArgumentException('$namespace cannot be null when creating version 3 UUIDs');
+        }
+
+        if ($name === null) {
+            throw new InvalidArgumentException('$name cannot be null when creating version 3 UUIDs');
+        }
+
+        /** @var non-empty-string $bytes */
+        $bytes = hash('md5', $namespace->toBytes() . $name, true);
+        $bytes = Util::applyVersionAndVariant($bytes, Version::HashMd5);
+
+        return new UuidV3($bytes);
+    }
+
+    public function createFromBytes(string $identifier): UuidV3
+    {
+        /** @var UuidV3 */
+        return $this->createFromBytesInternal($identifier);
+    }
+
+    public function createFromHexadecimal(string $identifier): UuidV3
+    {
+        /** @var UuidV3 */
+        return $this->createFromHexadecimalInternal($identifier);
+    }
+
+    public function createFromInteger(int | string $identifier): UuidV3
+    {
+        /** @var UuidV3 */
+        return $this->createFromIntegerInternal($identifier);
+    }
+
+    public function createFromString(string $identifier): UuidV3
+    {
+        /** @var UuidV3 */
+        return $this->createFromStringInternal($identifier);
+    }
+
+    protected function getVersion(): Version
+    {
+        return Version::HashMd5;
+    }
+
+    protected function getUuidClass(): string
+    {
+        return UuidV3::class;
+    }
+}
---
+++ src/Uuid/Factory/UuidV5Factory.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * This file is part of ramsey/identifier
+ *
+ * ramsey/identifier is open source software: you can distribute
+ * it and/or modify it under the terms of the MIT License
+ * (the "License"). You may not use this file except in
+ * compliance with the License.
+ *
+ * @copyright Copyright (c) Ben Ramsey <ben@benramsey.com>
+ * @license https://opensource.org/licenses/MIT MIT License
+ */
+
+declare(strict_types=1);
+
+namespace Ramsey\Identifier\Uuid\Factory;
+
+use Identifier\Uuid\UuidFactoryInterface;
+use Identifier\Uuid\UuidInterface;
+use Identifier\Uuid\Version;
+use Ramsey\Identifier\Exception\InvalidArgumentException;
+use Ramsey\Identifier\Uuid\Util;
+use Ramsey\Identifier\Uuid\UuidV5;
+
+use function hash;
+use function substr;
+
+/**
+ * A factory for creating version 5, name-based (SHA-1) UUIDs
+ *
+ * @psalm-immutable
+ */
+final class UuidV5Factory implements UuidFactoryInterface
+{
+    use DefaultFactory;
+
+    public function create(?UuidInterface $namespace = null, ?string $name = null): UuidV5
+    {
+        if ($namespace === null) {
+            throw new InvalidArgumentException('$namespace cannot be null when creating version 5 UUIDs');
+        }
+
+        if ($name === null) {
+            throw new InvalidArgumentException('$name cannot be null when creating version 5 UUIDs');
+        }
+
+        /** @var non-empty-string $bytes */
+        $bytes = substr(hash('sha1', $namespace->toBytes() . $name, true), 0, 16);
+        $bytes = Util::applyVersionAndVariant($bytes, Version::HashSha1);
+
+        return new UuidV5($bytes);
+    }
+
+    public function createFromBytes(string $identifier): UuidV5
+    {
+        /** @var UuidV5 */
+        return $this->createFromBytesInternal($identifier);
+    }
+
+    public function createFromHexadecimal(string $identifier): UuidV5
+    {
+        /** @var UuidV5 */
+        return $this->createFromHexadecimalInternal($identifier);
+    }
+
+    public function createFromInteger(int | string $identifier): UuidV5
+    {
+        /** @var UuidV5 */
+        return $this->createFromIntegerInternal($identifier);
+    }
+
+    public function createFromString(string $identifier): UuidV5
+    {
+        /** @var UuidV5 */
+        return $this->createFromStringInternal($identifier);
+    }
+
+    protected function getVersion(): Version
+    {
+        return Version::HashSha1;
+    }
+
+    protected function getUuidClass(): string
+    {
+        return UuidV5::class;
+    }
+}
