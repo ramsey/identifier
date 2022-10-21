bdaa76b2a1a4679bba83c52ffa6424635a8f2b30
Ben Ramsey <ben@benramsey.com>
2022-10-20 20:46:51 -0500

    feat: allow caching the random NIC address

--- src/Service/Nic/RandomNic.php
+++ src/Service/Nic/RandomNic.php
@@ -16,18 +16,73 @@ declare(strict_types=1);
 
 namespace Ramsey\Identifier\Service\Nic;
 
+use Psr\SimpleCache\CacheInterface;
+
 use function random_int;
 use function sprintf;
 
 /**
  * A NIC that generates a random MAC address and sets the multicast bit,
- * according to RFC 4122, section 4.5
+ * according to RFC 4122, section 4.5. The address is stored and reused for each
+ * call to address() within the same process. If a cache is provided, the same
+ * address is used across processes.
  *
  * @link https://datatracker.ietf.org/doc/html/rfc4122#section-4.5 Node IDs that Do Not Identify the Host
  */
 final class RandomNic implements Nic
 {
+    /**
+     * Key to use when caching the address value in a PSR-16 cache instance
+     */
+    private const CACHE_KEY = '__ramsey_id_random_addr';
+
+    /**
+     * The address, stored statically for better performance
+     *
+     * @var non-empty-string | null
+     */
+    private static ?string $address = null;
+
+    /**
+     * @param CacheInterface | null $cache An optional PSR-16 cache instance to
+     *     cache the address for faster lookups. Be aware that use of a
+     *     centralized cache might have unintended consequences if you wish to
+     *     use machine-specific addresses. If you wish for machine-specific
+     *     addresses, use of a machine-local cache, such as APCu, is preferable.
+     */
+    public function __construct(private readonly ?CacheInterface $cache = null)
+    {
+    }
+
     public function address(): string
+    {
+        if (self::$address === null) {
+            self::$address = $this->getAddressFromCache();
+        }
+
+        return self::$address;
+    }
+
+    /**
+     * @return non-empty-string
+     */
+    private function getAddressFromCache(): string
+    {
+        /** @var string | null $address */
+        $address = $this->cache?->get(self::CACHE_KEY);
+
+        if ($address === null || $address === '') {
+            $address = $this->generateAddress();
+            $this->cache?->set(self::CACHE_KEY, $address);
+        }
+
+        return $address;
+    }
+
+    /**
+     * @return non-empty-string
+     */
+    public function generateAddress(): string
     {
         /** @var non-empty-string */
         return sprintf('%06x%06x', random_int(0, 0xffffff) | 0x010000, random_int(0, 0xffffff));
--- tests/unit/Service/Nic/RandomNicTest.php
+++ tests/unit/Service/Nic/RandomNicTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Ramsey\Test\Identifier\Service\Nic;
 
+use Mockery;
+use Psr\SimpleCache\CacheInterface;
 use Ramsey\Identifier\Service\Nic\RandomNic;
 use Ramsey\Test\Identifier\TestCase;
 
@@ -13,6 +15,10 @@ use function substr;
 
 class RandomNicTest extends TestCase
 {
+    /**
+     * @runInSeparateProcess since the address is stored statically on the class
+     * @preserveGlobalState disabled
+     */
     public function testAddress(): void
     {
         $nic = new RandomNic();
@@ -23,5 +29,71 @@ class RandomNicTest extends TestCase
 
         // Assert the multicast bit is set.
         $this->assertSame(1, hexdec($firstOctet) & 0x01);
+
+        // Assert the address is the same for subsequent calls.
+        $this->assertSame($address, $nic->address());
+    }
+
+    /**
+     * @runInSeparateProcess since the address is stored statically on the class
+     * @preserveGlobalState disabled
+     */
+    public function testAddressFoundInCache(): void
+    {
+        $cache = $this->mockery(CacheInterface::class);
+        $cache->expects('get')->with('__ramsey_id_random_addr')->andReturn('aabbccddeeff');
+
+        $nic = new RandomNic($cache);
+
+        $this->assertSame('aabbccddeeff', $nic->address());
+
+        // This second assertion tests that the cache is not accessed again.
+        $this->assertSame('aabbccddeeff', $nic->address());
+    }
+
+    /**
+     * @runInSeparateProcess since the address is stored statically on the class
+     * @preserveGlobalState disabled
+     */
+    public function testAddressStoredInCache(): void
+    {
+        $cache = $this->mockery(CacheInterface::class);
+        $cache->expects('get')->with('__ramsey_id_random_addr')->andReturnNull();
+        $cache
+            ->expects('set')
+            ->with('__ramsey_id_random_addr', Mockery::pattern('/^[0-9a-f]{12}$/i'))
+            ->andReturnTrue();
+
+        $nic = new RandomNic($cache);
+        $address = $nic->address();
+
+        $this->assertSame(12, strlen($address));
+
+        // This second assertion tests that the cache get() and set() methods
+        // are not called again, and that we still get the same address.
+        $this->assertSame($address, $nic->address());
+    }
+
+    /**
+     * @runInSeparateProcess since the address is stored statically on the class
+     * @preserveGlobalState disabled
+     */
+    public function testAddressStoredInCacheIsSomehowAnEmptyString(): void
+    {
+        $cache = $this->mockery(CacheInterface::class);
+        $cache->expects('get')->with('__ramsey_id_random_addr')->andReturn('');
+        $cache
+            ->expects('set')
+            ->with('__ramsey_id_random_addr', Mockery::pattern('/^[0-9a-f]{12}$/i'))
+            ->andReturnTrue();
+
+        $nic = new RandomNic($cache);
+        $address = $nic->address();
+
+        $this->assertSame(12, strlen($address));
+
+        // This second assertion tests that the cache get() and set() methods
+        // are not called again, and that we still get the same address.
+        $this->assertSame($address, $nic->address());
     }
 }
